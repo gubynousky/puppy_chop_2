e5a6f60741609bea2dd812cc108e31488f5ff693
gubynousky <ru.martinezs@duocuc.cl>
2025-10-22 23:04:41 -0300

    Se crea la media query para la responsividad del sitio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import CatalogoPage from './pages/CatalogoPage';
